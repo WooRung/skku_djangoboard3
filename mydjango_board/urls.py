aee6b720d8bf0c056ef1f688d67ba765a23fd095
younsoo <tweatershin@gmail.com>
2023-06-07 17:55:54 +0900

    static file serving

--- mydjango_board/urls.py
+++ mydjango_board/urls.py
@@ -16,6 +16,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
+from django.conf import settings
+from django.conf.urls.static import static
+
 """
 # http://127.0.0.1:8000/user
 # 1. /user로 요청이들어오면 <project>/urls.py 로 접근하여 일치하는 url을 찾는다.
@@ -27,4 +30,4 @@ urlpatterns = [
     path("admin/", admin.site.urls),
     path('board/', include('board.urls')),
     path('user/', include('user.urls')),
-]
+] + static(settings.STATIC_URL, document_root = settings.STATIC_ROOT)
