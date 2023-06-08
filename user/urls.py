aa0a24b3eedb5e96026ea2a2be44dd93d8b4b137
younsoo <tweatershin@gmail.com>
2023-06-08 13:38:32 +0900

    django register 구현

--- user/urls.py
+++ user/urls.py
@@ -10,4 +10,6 @@ urlpatterns = [
     path('login', auth_views.LoginView.as_view(
         template_name='user/login.html'), name='login'),
     path('logout', auth_views.LogoutView.as_view(), name='logout'),
+    path('register', views.register, name='register'),
+    # path('sample', views.SampleView.as_view())
 ]
--- user/views.py
+++ user/views.py
@@ -6,18 +6,59 @@
 # 3. viewFunction에서는 request를 받아 response를 반환한다.
 """
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.views.generic.base import View
+
+from django.contrib.auth import models
+
+# from django.conf import settings
+from .forms import RegisterForm
+
+from django.contrib.auth import authenticate, login
+
 
 def index(request):
+  print(request.user)
+  print(request.user.is_authenticated)
+  print(request.user.is_superuser)
+  print(request.user.is_staff)
   return render(request, 'user/profile.html')
 
-def user_profile(request):
-    return HttpResponse(
-      """
-      <h1>나의 프로필</h1>
-      <ul>
-        <li>이름: 신윤수</li>
-        <li>별명: ys</li>
-      </ul>
-      """
-    )
+def register(request):
+  form = RegisterForm()
+  if request.method == 'POST':
+    form = RegisterForm(request.POST)
+    if form.is_valid():
+      form.save()
+      username = form.cleaned_data.get('username')
+      password = form.cleaned_data.get('password1')
+      print(username)
+      print(password)
+      user = authenticate(username=username, password=password) # DB의 유저정보와 비교 및 조회
+      login(request, user) # 실질적인 로그인
+      return redirect(reverse('user:index'))
+
+  return render(request, 'user/register.html', {'form': form})
+
+# class SampleView(View):
+#   def get(self, request, *args, **kwargs):
+#     return HttpResponse('get요청')
+#   def post(self, request, *args, **kwargs):
+#     return HttpResponse('post요청')
+    
+#   def put(self, request, *args, **kwargs):
+#     return HttpResponse('put요청')
+
+#   def delete(self, request, *args, **kwargs):
+#     return HttpResponse('delete요청')
+
+# def user_profile(request):
+#     return HttpResponse(
+#       """
+#       <h1>나의 프로필</h1>
+#       <ul>
+#         <li>이름: 신윤수</li>
+#         <li>별명: ys</li>
+#       </ul>
+#       """
+#     )
