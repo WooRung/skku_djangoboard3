1e0174f2d820ef3f9aa0631aa60da0f94f41a0ff
BeTweak <ys@betweak.com>
2023-06-21 23:25:14 +0900

    setting chagne

--- mydjango_board/settings.py
+++ mydjango_board/settings.py
@@ -79,7 +79,7 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'django_myboard',
-        'USER': 'root',
+        'USER': 'admin',
         'PASSWORD': 'admin1234',
         'HOST': '127.0.0.1',
         'PORT': '3306',
